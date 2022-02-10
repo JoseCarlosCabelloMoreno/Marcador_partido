c6dfcba305b221ae3d40b682e96da584665099d8
joseCarlosCabello <tsunayoshi598@gmail.com>
2022-02-10 14:24:08 -0600

    Funciones de suma y resta y reset

--- app/src/main/java/com/example/marcador/MainActivity.kt
+++ app/src/main/java/com/example/marcador/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.marcador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.marcador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         val boton_maenos_visitante=binding.botonMenosVisitante
         val marcador_local=binding.LocalMarcador
         val marcador_visitante=binding.VisitanteMarcador
+        val restart_boton=binding.botonReset
 
 
         val Marcador=marcador(marcador_local.text.toString(),marcador_visitante.text.toString())
@@ -27,10 +29,66 @@ class MainActivity : AppCompatActivity() {
         boton_mas_2_local.setOnClickListener{
             binding.LocalMarcador.text=sumar2("local", Marcador)
         }
+        boton_mas_local.setOnClickListener{
+            binding.LocalMarcador.text=sumar("local",Marcador)
+        }
+        boton_menos_local.setOnClickListener {
+            if (Marcador.local.toInt()<=0)
+            {
+                Toast.makeText(this, "No se puede restar a local", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                binding.LocalMarcador.text=restar("local",Marcador)
+            }
+
+        }
+
+
+
+
         boton_mas_2_visitante.setOnClickListener{
             binding.VisitanteMarcador.text=sumar2("visitante", Marcador)
         }
+        boton_mas_visitante.setOnClickListener{
+            binding.VisitanteMarcador.text=sumar("visitante",Marcador)
+        }
+        boton_maenos_visitante.setOnClickListener{
+            if (Marcador.visitante.toInt()<=0)
+            {
+                Toast.makeText(this, "No se puede restar a visitante", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                binding.VisitanteMarcador.text=restar("visitante",Marcador)
+            }
     }
+
+        restart_boton.setOnClickListener {
+            binding.VisitanteMarcador.text=reiniciar(Marcador)
+            binding.LocalMarcador.text=reiniciar(Marcador)
+        }
+    }
+
+    private fun reiniciar(Marcador: marcador): String? {
+        val re= "0"
+        Marcador.local=re
+        Marcador.visitante=re
+        return re
+    }
+
+    private fun sumar(vis: String,Marcador: marcador): String? {
+        if (vis=="local")
+        {
+            val local_sum=Marcador.local.toInt()+1
+            Marcador.local=local_sum.toString()
+            return local_sum.toString()
+        }
+        else{
+            val visit_sum=Marcador.visitante.toInt()+1
+            Marcador.visitante=visit_sum.toString()
+            return visit_sum.toString()
+        }
+    }
+
 
     private fun sumar2(vis:String, Marcador: marcador): String {
         if (vis=="local")
@@ -46,5 +104,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun restar(vis:String,Marcador: marcador): String? {
 
+            if (vis=="local")
+            {
+                val local_sum=Marcador.local.toInt()-1
+                Marcador.local=local_sum.toString()
+                return local_sum.toString()
+            }
+
+        else{
+            val visit_sum=Marcador.visitante.toInt()-1
+            Marcador.visitante=visit_sum.toString()
+            return visit_sum.toString()
+        }
+    }
 }
+
+
