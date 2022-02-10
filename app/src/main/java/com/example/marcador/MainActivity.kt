ae59a21baf9fdc01032130df28f7e6f8192fe521
joseCarlosCabello <tsunayoshi598@gmail.com>
2022-02-10 09:50:14 -0600

    Se agrego biding

--- app/src/main/java/com/example/marcador/MainActivity.kt
+++ app/src/main/java/com/example/marcador/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.marcador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.marcador.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    val binding=ActivityMainBinding.inflate(layoutInflater)
+
+    val boton_mas_local=binding.boton1Local
+    val boton_mas_2_local=binding.boton2Local
+    val boton_menos_local=binding.botonMenosLocal
+    val boton_mas_visitante=binding.boton1Visitante
+    val boton_mas_2_visitante=binding.boton2Visitante
+    val boton_maenos_visitante=binding.botonMenosVisitante
+
+    val marcador_local=binding.LocalMarcador
+    val marcador_visitante=binding.VisitanteMarcador
 }
---
+++ app/src/main/java/com/example/marcador/marcador.kt
@@ -0,0 +1,8 @@
+package com.example.marcador
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class marcador(val local:String,val visitante:String) : Parcelable {
+}
